3486e06c5912dc6731fc42698c54ea1013c5c50f
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-04 20:30:50 +0800

    Update ScheduledBooking.jsx
    client button

--- src/components/Profile/ScheduledBooking.jsx
+++ src/components/Profile/ScheduledBooking.jsx
@@ -90,7 +90,6 @@ const ScheduledBooking = () => {
                         <div className="btn-group">
                           <Button disabled={status === 'pending' ? false : true} onClick={() => handleShow({bookingId: booking_id, status: "in_progress"})} variant="info" ><i className="fas fa-spinner"></i></Button>
                           <Button disabled={status === 'in_progress' ? false : true} onClick={() => handleShow({bookingId: booking_id, status: "done"})} variant="success" ><i className="fas fa-check"></i></Button>
-                          <Button disabled={status === 'in_progress' ? false : true} onClick={() => handleShow({bookingId: booking_id, status: "cancelled"})} variant="danger" ><i className="fas fa-times"></i></Button>
                         </div>
                       </td>
                     </tr>)
