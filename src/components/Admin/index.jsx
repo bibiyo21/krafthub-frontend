07e4e11e3901472b96d7b937f74951c0d9af27fc
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-18 18:17:55 +0800

    Update index.jsx
    ++ import

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Card, Button, Modal, Form } from "react-bootstrap";
 import Wrapper from "./Wrapper";
 import UserServiceAPI from "../../api/services/Users/UsersService";
+import pdf from '../pdf'
 
 
 const Admin = () => {
