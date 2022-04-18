585ad5a95d43b9af5f0dcfb7f5a81aa0ee4ca352
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-18 19:51:58 +0800

    Update index.jsx
    temp

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Card, Button, Modal, Form } from "react-bootstrap";
 import Wrapper from "./Wrapper";
 import UserServiceAPI from "../../api/services/Users/UsersService";
+import BookingsServiceAPI from "../../api/services/Bookings/BookingsService";
 import * as dayjs from 'dayjs'
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
