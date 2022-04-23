3ed754e3a9217322f8b60c7588338b468fc7066a
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-23 22:39:00 +0800

    Update Home.jsx
    ++ admin page dashboard

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import AvailabilityList from "../AvailabilityList";
 import Navigations from "../Navigations";
 import SearchBar from "../SearchBar";
+import Admin  from "../Admin";
+import AuthenticationAPI from "../../api/services/Authentication/AuthenticationService";
 
 const Home = () => {
   const [availabilityList, setAvailabilityList] = useState(null);
-  return (
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  if(user.access_level === 4) {
+      return (
+    <>
+      <Admin />
+    </>
+  );
+  } else {
+    
+      return (
     <>
       <Navigations />
       <div className="container pt-3">
@@ -14,6 +26,10 @@ const Home = () => {
       <AvailabilityList list={availabilityList} />
     </>
   );
+    
+  }
+  
+
 };
 
 export default Home;
