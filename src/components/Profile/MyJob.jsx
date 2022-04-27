ce8f85a063eaf886136903642310b3327177b04f
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-27 21:58:02 +0800

    Update MyJob.jsx
     amount

--- src/components/Profile/MyJob.jsx
+++ src/components/Profile/MyJob.jsx
@@ -1,51 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { Card, Form, Button, Alert } from "react-bootstrap";
-import { useForm } from "react-hook-form";
-import JobsServiceAPI from "../../api/services/Jobs/JobsService";
-import AvailabilitiesServiceAPI from "../../api/services/Availabilities/AvailabilitiesService";
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from 'react-dom';
+import { Card, Button, Modal, Form } from "react-bootstrap";
 import Wrapper from "./Wrapper";
+import BookingsServiceAPI from "../../api/services/Bookings/BookingsService";
+import * as dayjs from 'dayjs'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const MyJob = () => {
-  const { register, handleSubmit } = useForm();
-  const [myJobs, setMyJobs] = useState(null);
-  const [jobs, setJobs] = useState(null);
-  const [jobTypes, setJobTypes] = useState(null);
-  const [savingMessage, setSavingMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
 
-  const onJobSearch = (e) => {
-    e.preventDefault();
-    JobsServiceAPI.getByTypes(e.target.value).then(({ results }) => {
-      setJobTypes(results);
+const ScheduledBooking = () => {
+  const STATUS_ATTR = {
+    pending: {color: 'text-primary', msg: "Pending"},
+    done: {color: 'text-success', msg: "Done"},
+    paid: {color: 'text-warning', msg: "Paid"},
+    cancelled: {color: 'text-danger', msg: "Cancelled"},
+    in_progress: {color: 'text-info', msg: "In Progress"},
+  }
+  const [show, setShow] = useState(false);
+  const [scheduledBookings, setScheduledBookings] = useState(null);
+  const [bookingState, setBookingState] = useState(null);
+  const [bookingId, setBookingId] = useState(null);
+  
+
+  const handleClose = () => {
+    setShow(false)
+    setBookingState(null)
+    setBookingId(null);
+  };
+
+  const handleShow = ({bookingId, status}) => {
+    setShow(true)
+    setBookingState(status)
+    
+    console.log(bookingState);
+    setBookingId(bookingId);
+  };
+
+  const loadScheduledBooking = () => {
+    BookingsServiceAPI.getScheduled().then(({ results }) => {
+      
+        console.log(results);
+      setScheduledBookings(results);
+      
+      
     })
   }
 
-  const onSubmit = ({ jobType: job_id, time_in, time_out, amount }) => {
-    setLoading(true);
-    JobsServiceAPI.saveAvailability({ job_id, time_in, time_out, amount})
-      .then((response) => {
-        setSavingMessage({responseType: "success",  message: response.message})
-        AvailabilitiesServiceAPI.getMyJobs().then(({ results }) => {
-          setMyJobs(results);
-        });
-      })
-      .catch(({ response }) => {
-        if (response?.data?.message !== undefined) {
-          setSavingMessage({responseType: "danger",  message: response.data.message});
-        }
-      }).finally(() => {
-        setLoading(false)
-      });
-  };
+  const onChangeStatus = () => {
+    BookingsServiceAPI.updateBookingStatus({
+      id: bookingId,
+      status: bookingState,
+    }).then((data) => {
+      toast.success(data.message);
+      handleClose();
+      loadScheduledBooking()
+    })
+  }
 
   useEffect(() => {
-    AvailabilitiesServiceAPI.getMyJobs().then(({ results }) => {
-      setMyJobs(results);
-    });
-
-    JobsServiceAPI.get().then(({ results }) => {
-      setJobs(results);
-    })
+    loadScheduledBooking()
   }, []);
 
   return (
@@ -53,92 +66,71 @@ const MyJob = () => {
       <Wrapper>
         <Card className="mb-4">
           <Card.Body>
-            <Card.Title >
-              <strong className="pl-3">Add Job</strong>
-            </Card.Title>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              {
-                savingMessage && <Alert variant={savingMessage.responseType} onClose={() => setSavingMessage(null)} dismissible>
-                  <p>{savingMessage.message}</p>
-                </Alert>
-              }
-
-              <Form.Group className="mb-3">
-                <select defaultValue="" className="form-select" {...register("job")} onChange={onJobSearch}>
-                  <option value="" disabled >Choose a Profession</option>
-                  {
-                    jobs && jobs.map((job, index) => {
-                      return <option key={`job-${index}`} value={job.id}>{job.title}</option>
-                    })
-                  }
-                </select>
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <select defaultValue="" className="form-select" {...register("jobType")}>
-                  <option value="" disabled >Choose a Specialist</option>
-                  {
-                    jobTypes && jobTypes.map((job, index) => {
-                      return <option key={`jobtype-${index}`} value={job.id}>{job.title}</option>
-                    })
-                  }
-                </select>
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <input type="text" className="form-control" placeholder="Time In - HH:MM AM/PM" {...register("time_in")} />
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <input type="text" className="form-control" placeholder="Time out - HH:MM AM/PM" {...register("time_out")} />
-              </Form.Group>
-              
-               <Form.Group className="mb-3">
-                <input type="text" className="form-control" placeholder="Payment (0.00)" {...register("amount")} />
-              </Form.Group>
-
-              <Button variant="primary" type="submit" disabled={loading}>
-                Save
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-
-        <Card>
-          <Card.Body>
-            <Card.Title>My Jobs</Card.Title>
+            <Card.Title>Scheduled Bookings</Card.Title>
             <table className="table table-responsive table-condensed table-striped table-hover">
               <thead>
                 <tr>
-                  <th>Job Type</th>
-                  <th>Job</th>
-                  <th>Availability</th>
-                  <th>Payment</th>
+                  <th>Maker</th>
+                  <th>Status</th>
+                  <th>Schedule</th>
+                  <th>Description</th>
+                  <th>Action</th>
+                   <th>Payment</th>
                 </tr>
               </thead>
               <tbody>
                 {
-                  myJobs !== null && myJobs.length > 0
-                  ? myJobs.map((job) => {
-                    return (
-                        <tr>
-                          <td>{job.profession}</td>
-                          <td>{job.specialty}</td>
-                          <td>{job.time_in} to {job.time_out}</td>
-                          <td>{job.amount}</td>
-                          {/* <td><Button variant="danger"><i className="fas fa-trash"></i></Button></td> */}
-                        </tr>
-                      )
-                    })
-                  : <td colSpan={3}>You have no jobs yet</td>
+                  scheduledBookings && scheduledBookings.map(({
+                    booking_id, first_name, last_name, status, eta, additional_info, amount
+                  }) => {
+                    return (<tr>
+                      <td>{first_name} {last_name}</td>
+                      <td><span className={STATUS_ATTR[status].color}>{STATUS_ATTR[status].msg}</span></td> 
+                      <td>{dayjs(eta).format('MMMM DD, YYYY HH:mm')}</td>
+                      <td>{additional_info}</td>
+                      <td>{amount}</td>
+                      <td>
+                        <div className="btn-group">
+                           <Button disabled={status === 'in_progress' || status === 'pending' ? false : true} onClick={() => handleShow({bookingId: booking_id, status: "cancelled"})} variant="danger" >Cancel<i className="fas fa-times"></i></Button>
+                        </div>
+                      </td>
+                    </tr>)
+                  })
                 }
+                
               </tbody>
             </table>
           </Card.Body>
         </Card>
       </Wrapper>
+      <BookingModal show={show} handleClose={handleClose} status={STATUS_ATTR?.[bookingState]?.msg} onChangeStatus={onChangeStatus} />
     </>
   );
 };
 
-export default MyJob;
+const BookingModal = ({ show, handleClose, status, onChangeStatus }) => {
+  return ReactDOM.createPortal(
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Booking Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to set this booking to <b>{status}</b>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => onChangeStatus()}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <ToastContainer />
+    </>,
+    document.getElementById('modal')
+  );
+}
+
+export default ScheduledBooking;
