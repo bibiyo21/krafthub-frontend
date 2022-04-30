b72705d181b11fe6718c0e78643c77eef3281f28
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-30 22:21:38 +0800

    Update MyJob.jsx
    ++

--- src/components/Profile/MyJob.jsx
+++ src/components/Profile/MyJob.jsx
@@ -123,8 +123,12 @@ const MyJob = () => {
               </Form.Group>
               
               <Form.Group className="mb-3">
-                <input type="file" class="form-control" onChange={onChangeFile} required name="image" placeholder="Add QR Code..." {...register("file_path")} />
-              </Form.Group>
+               <input type="file" class="form-control"   onChange={(event) => {
+                                  console.log(event.target.files[0]);
+                                  setSelectedImage(event.target.files[0]);
+                                }} required name="image" placeholder="Add QR Code..." {...register("file_path")} />
+                                
+               </Form.Group>
               
           
 
