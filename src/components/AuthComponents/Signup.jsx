e6b294075c174cf47a74d901e3b1ce078db884fe
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-09 01:37:10 +0800

    Update Signup.jsx

--- src/components/AuthComponents/Signup.jsx
+++ src/components/AuthComponents/Signup.jsx
@@ -78,8 +78,6 @@ const  Signup = () => {
           {errors?.email !== undefined && <p className="text-danger">{errors.email[0]}</p>}
         </Form.Group>
 
-          <a hidden name ="message" href="https://drfayot.herokuapp.com/confirm">https://drfayot.herokuapp.com/confirm </a>
-
         <Form.Group className="mb-3">
           <input type="text"  maxlength="15" className="form-control" defaultCountry="PH" placeholder="Mobile Number - 639XX-XXXX-XXX" {...register("cellphone_number", { required: true })} />
           {errors?.cellphone_number !== undefined && <p className="text-danger">{errors.cellphone_number[0]}</p>}
