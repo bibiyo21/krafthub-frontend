687b1932b9b1a2dd9478ed9f33f3b303c081c02f
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-09 11:29:06 +0800

    Update Signup.jsx

--- src/components/AuthComponents/Signup.jsx
+++ src/components/AuthComponents/Signup.jsx
@@ -8,7 +8,33 @@ import emailjs from '@emailjs/browser';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+function formatPhoneNumber(value) {
+  // if input value is falsy eg if the user deletes the input, then just return
+  if (!value) return value;
 
+  // clean the input for any non-digit values.
+  const phoneNumber = value.replace(/[^\d]/g, "");
+
+  // phoneNumberLength is used to know when to apply our formatting for the phone number
+  const phoneNumberLength = phoneNumber.length;
+
+  // we need to return the value with no formatting if its less then four digits
+  // this is to avoid weird behavior that occurs if you  format the area code to early
+  if (phoneNumberLength < 4) return phoneNumber;
+
+  // if phoneNumberLength is greater than 4 and less the 7 we start to return
+  // the formatted number
+  if (phoneNumberLength < 8) {
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+  }
+
+  // finally, if the phoneNumberLength is greater then seven, we add the last
+  // bit of formatting and return it.
+  return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+    3,
+    6
+  )}-${phoneNumber.slice(6, 12)}`;
+}
 
 const  Signup = () => {
   const { 
@@ -20,34 +46,6 @@ const  Signup = () => {
   
   const [inputValue, setInputValue] = useState("");
 
-  const formatPhoneNumber = ({ mobileValue }) =>  {
-  // if input value is falsy eg if the user deletes the input, then just return
-  if (!mobileValue) return mobileValue;
-
-  // clean the input for any non-digit values.
-  const phoneNumber = mobileValue.replace(/[^\d]/g, "");
-
-  // phoneNumberLength is used to know when to apply our formatting for the phone number
-  const phoneNumberLength = phoneNumber.length;
-
-  // we need to return the value with no formatting if its less then four digits
-  // this is to avoid weird behavior that occurs if you  format the area code to early
-  if (phoneNumberLength < 4) return phoneNumber;
-
-  // if phoneNumberLength is greater than 4 and less the 7 we start to return
-  // the formatted number
-  if (phoneNumberLength < 7) {
-    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
-  }
-
-  // finally, if the phoneNumberLength is greater then seven, we add the last
-  // bit of formatting and return it.
-  return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
-    3,
-    6
-  )}-${phoneNumber.slice(6, 10)}`;
-}
-  
   const handleInput = (e) => {
     // this is where we'll call the phoneNumberFormatter function
     const formattedPhoneNumber = formatPhoneNumber(e.target.value);
