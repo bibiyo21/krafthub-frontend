a59e6ceca98b4a62a8655d02a24d4d01abc1822d
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-14 13:36:32 +0800

    Update SearchBar.jsx
    ++

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,137 +4,20 @@ import { useForm } from "react-hook-form";
 import AvailabilitiesServiceAPI from "../api/services/Availabilities/AvailabilitiesService";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import * as dayjs from 'dayjs'
 
 const SearchBar = ({
   setAvailabilityResult = null
 }) => {
   const [jobs, setJobs] = useState(null);
   const [jobTypes, setJobTypes] = useState(null);
-  const [jobID, setJobID] = useState(null);
-  
-  const [timeSearchVal, setTimeSearchVal] = useState(null);
-  
+
   const { register, handleSubmit } = useForm();
-  const onSearch = ({ userName, job, jobType, timeSearchIn, timeSearchOut }) => {
-    
-    if(timeSearchIn !== "Select time:" && timeSearchOut !== "Select time:") {
-      
-         AvailabilitiesServiceAPI.get({}).then(({ results }) => {
-
-              setTimeSearchVal(results);
-
-          });
-
-              console.log(timeSearchVal);
-      
-         const timeResult = timeSearchVal ?
-               
-               timeSearchVal.filter(function(timeSearchVal){ return (timeSearchVal.time_in.toLowerCase() === timeSearchIn.toLowerCase()) && 
-                                                                                    (timeSearchVal.time_out.toLowerCase() === timeSearchOut.toLowerCase())
-                                                            })
-                : [];
-      
-      
-         setAvailabilityResult(timeResult);
-      
-      
-    } else if (timeSearchIn !== "Select time:") {
-      
-      AvailabilitiesServiceAPI.get({}).then(({ results }) => {
-
-              setTimeSearchVal(results);
-
-          });
-
-              console.log(timeSearchVal);
-      
-         setAvailabilityResult(timeSearchVal.filter(function(timeSearchVal){ return (timeSearchVal.time_in.toLowerCase() === timeSearchIn.toLowerCase()) 
-                                                                            }));
-      
-    } else if (timeSearchOut !== "Select time:") {
-      
-      AvailabilitiesServiceAPI.get({}).then(({ results }) => {
-
-              setTimeSearchVal(results);
-
-          });
-
-              console.log(timeSearchVal);
-      
-         setAvailabilityResult(timeSearchVal.filter(function(timeSearchVal){ return (timeSearchVal.time_out.toLowerCase() === timeSearchOut.toLowerCase()) 
-                                                                            }));
-      
-    } else { 
-      
-            AvailabilitiesServiceAPI.get({ userName, job, jobType }).then(({ results }) => {
-              if (setAvailabilityResult !== null) {
-
-                    if (results.length !== 0) {
-                      setAvailabilityResult(results);
-                    } else {
-
-                          job = jobs.filter(function(jobs){ return jobs.title.toLowerCase() === userName.toLowerCase() });
-                          if(job.length !== 0) {
-
-                              job = job[0].id;
-                              userName = "";
-                               AvailabilitiesServiceAPI.get({ userName, job, jobType }).then(({ results }) => {    
-                                  if (setAvailabilityResult !== null) {
-                                      if (results.length !== 0) {
-                                      setAvailabilityResult(results);
-                                      } else {
-                                        toast.success("No Data Found.");
-                                      }
-                                    } 
-
-                                })
-
-                          } else { 
-
-
-                              if(jobTypes !== null) {
-                                 jobType = jobTypes.filter(function(jobTypes){ return jobTypes.title.toLowerCase() === userName.toLowerCase() });
-
-
-                                if(jobType.length !== 0 ) {
-
-                                    jobType = jobType[0].id;
-                                   job = "";
-                                userName = "";
-                                 AvailabilitiesServiceAPI.get({ userName, job, jobType }).then(({ results }) => {    
-                                    if (setAvailabilityResult !== null) {
-
-                                         if (results.length !== 0) {
-                                            setAvailabilityResult(results);
-                                           } 
-
-                                      } 
-
-                                  })
-                              } else {
-                                             toast.warning("No Data Found.");
-                                             return;
-                               }
-
-
-
-                            }    
-
-                          } 
-
-
-
-                    }
-
-              } 
-
-
-
-            })
-      
-    }
-
+  const onSearch = ({ userName, job, jobType }) => {
+    AvailabilitiesServiceAPI.get({ userName, job, jobType }).then(({ results }) => {
+      if (setAvailabilityResult !== null) {
+        setAvailabilityResult(results);
+      }
+    })
   };
 
   const onJobSearch = (e) => {
@@ -143,7 +26,7 @@ const SearchBar = ({
       setJobTypes(results);
     })
   }
- 
+  
 
   useEffect(() => {
     AvailabilitiesServiceAPI.get({}).then(({ results }) => {
@@ -152,11 +35,6 @@ const SearchBar = ({
       }
     })
 
-    
-    JobsServiceAPI.getAllJobTypes().then(({ results }) => {
-      setJobTypes(results);
-    })
-    
     JobsServiceAPI.get().then(({ results }) => {
       setJobs(results);
     })
@@ -171,89 +49,30 @@ const SearchBar = ({
               <div class="col-lg-12">
                 <form className="p-3" id="contact" onSubmit={handleSubmit(onSearch)}>
                   <div class="row">
+                   <div class="col-lg-3">
+                      <fieldset>
+                        <select defaultValue="" className="form-select" {...register("job")} onChange={onJobSearch}>
+                          <option value="" disabled >Choose a Profession</option>
+                          {
+                            jobs && jobs.map((job, index) => {
+                              return <option key={`job-${index}`} value={job.id}>{job.title}</option>
+                            })
+                          }
+                        </select>
+                      </fieldset>
+                    </div>
                     <div class="col-lg-3">
                       <fieldset>
-                        <input type="text" className="form-control" placeholder="Search Maker" {...register("userName")} />
+                      <select defaultValue="" className="form-select" {...register("jobType")}>
+                        <option value="" disabled >Choose a Specialist</option>
+                        {
+                          jobTypes && jobTypes.map((job, index) => {
+                            return <option key={`jobtype-${index}`} value={job.id}>{job.title}</option>
+                          })
+                        }
+                      </select>
                       </fieldset>
-                      
                     </div>
-                   <div class="col-lg-3">
-                        <fieldset>
-                           <select id="TimePicker" name="TimePicker"  {...register("timeSearchIn")} >
-                                                 <option value="Select time:">Select Time In:</option>
-                                               <option value="8:00 AM">8:00 AM</option>
-                                                        <option value="8:30 AM">8:30 AM</option>
-                                                        <option value="9:00 AM">9:00 AM</option>
-                                                        <option value="9:30 AM">9:30 AM</option>
-                                                        <option value="10:00 AM">10:00 AM</option>
-                                                        <option value="10:30 AM">10:30 AM</option>
-                                                        <option value="11:00 AM">11:00 AM</option>
-                                                        <option value="11:30 AM">11:30 AM</option>
-                                                        <option value="12:00 NN">12:00 NN</option>
-                                                        <option value="12:30 NN">12:30 NN</option>
-                                                        <option value="01:00 PM">01:00 PM</option>
-                                                        <option value="01:30 PM">01:30 PM</option>
-                                                        <option value="02:00 PM">02:00 PM</option>
-                                                        <option value="02:30 PM">02:30 PM</option>
-                                                        <option value="03:00 PM">03:00 PM</option>
-                                                        <option value="03:30 PM">03:30 PM</option>
-                                                        <option value="04:00 PM">04:00 PM</option>
-                                                        <option value="04:30 PM">04:30 PM</option>
-                                                        <option value="05:00 PM">05:00 PM</option>
-                                                        <option value="05:30 PM">05:30 PM</option>
-                                                        <option value="06:00 PM">06:00 PM</option>
-                                                        <option value="06:30 PM">06:30 PM</option>
-                                                        <option value="07:00 PM">07:00 PM</option>
-                                                        <option value="07:30 PM">07:30 PM</option>
-                                                        <option value="08:00 PM">08:00 PM</option>
-                                                        <option value="08:30 PM">08:30 PM</option>
-                                                        <option value="09:00 PM">09:00 PM</option>
-                                                        <option value="09:30 PM">09:30 PM</option>
-                                                        <option value="10:00 PM">10:00 PM</option>
-                                                        <option value="10:30 PM">10:30 PM</option>
-                                              </select>
-                             </fieldset>
-                        </div>
-
-                       <div class="col-lg-3">
-                        <fieldset>
-                           <select id="TimePicker" name="TimePicker"  {...register("timeSearchOut")} >
-                                                 <option value="Select time:">Select Time Out:</option>
-                                               <option value="8:00 AM">8:00 AM</option>
-                                                        <option value="8:30 AM">8:30 AM</option>
-                                                        <option value="9:00 AM">9:00 AM</option>
-                                                        <option value="9:30 AM">9:30 AM</option>
-                                                        <option value="10:00 AM">10:00 AM</option>
-                                                        <option value="10:30 AM">10:30 AM</option>
-                                                        <option value="11:00 AM">11:00 AM</option>
-                                                        <option value="11:30 AM">11:30 AM</option>
-                                                        <option value="12:00 NN">12:00 NN</option>
-                                                        <option value="12:30 NN">12:30 NN</option>
-                                                        <option value="01:00 PM">01:00 PM</option>
-                                                        <option value="01:30 PM">01:30 PM</option>
-                                                        <option value="02:00 PM">02:00 PM</option>
-                                                        <option value="02:30 PM">02:30 PM</option>
-                                                        <option value="03:00 PM">03:00 PM</option>
-                                                        <option value="03:30 PM">03:30 PM</option>
-                                                        <option value="04:00 PM">04:00 PM</option>
-                                                        <option value="04:30 PM">04:30 PM</option>
-                                                        <option value="05:00 PM">05:00 PM</option>
-                                                        <option value="05:30 PM">05:30 PM</option>
-                                                        <option value="06:00 PM">06:00 PM</option>
-                                                        <option value="06:30 PM">06:30 PM</option>
-                                                        <option value="07:00 PM">07:00 PM</option>
-                                                        <option value="07:30 PM">07:30 PM</option>
-                                                        <option value="08:00 PM">08:00 PM</option>
-                                                        <option value="08:30 PM">08:30 PM</option>
-                                                        <option value="09:00 PM">09:00 PM</option>
-                                                        <option value="09:30 PM">09:30 PM</option>
-                                                        <option value="10:00 PM">10:00 PM</option>
-                                                        <option value="10:30 PM">10:30 PM</option>
-                                              </select>
-                             </fieldset>
-                        </div>
-
-                   
                     <div class="col-lg-3">
                       <fieldset className="text-center">
                         <button type="submit" id="form-submit" class="button">
