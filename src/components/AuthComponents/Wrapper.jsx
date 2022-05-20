aa50924ce430e6a23d5c74282f565334d8bc8099
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-20 22:17:52 +0800

    Update Wrapper.jsx

--- src/components/AuthComponents/Wrapper.jsx
+++ src/components/AuthComponents/Wrapper.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import logoImg from '../../images/logo.jpg';
+import tagLineImg from '../../images/TagLine.jpg';
+import nameImg from '../../images/NameTag.jpg';
 
 const Wrapper = ({ children }) => {
   if (localStorage.getItem('token')) {
@@ -9,7 +12,7 @@ const Wrapper = ({ children }) => {
     
     <div>
         <h2 class="text-left mb-3">
-          <img class="_1-kh _93s6 img" src="../../images/logo.jpg" alt="LogoKH" />
+          <img class="_1-kh _93s6 img" src={logoImg} alt="LogoKH" />
             <ul class="_2fux">
                 <li class="_4qfn"><a href="https://krafthub-live.web.app/AboutUs" class="_4vo5">About Us  </a>                                                           
                 </li>
@@ -20,7 +23,7 @@ const Wrapper = ({ children }) => {
             </ul>
         </h2>
     <div className="auth-wrapper">
-              <img class="_1-kj _93s6 img" src="../../images/TagLine.jpg" alt="TagLineKH" />
+              <img class="_1-kj _93s6 img" src={tagLineImg} alt="TagLineKH" />
               <div className="auth-inner">
                 {children}
               </div>
@@ -38,7 +41,7 @@ const Wrapper = ({ children }) => {
                     <span class="_6aje"> Krafthub is your one-stop booking hub for home improvement and maintenance services </span>
                     <span class="_6aje">
                        <div style="max-width: 150px; margin: 0 auto;">
-                          <img class="_6b6a img" alt="Krafthub" src="../../images/NameTag.jpg" width="161" alt="NameTagKH" />
+                          <img class="_1-kh _93s6 img" alt="Krafthub" src={nameImg} width="161" alt="NameTagKH" />
                        </div>
                     </span>
   
