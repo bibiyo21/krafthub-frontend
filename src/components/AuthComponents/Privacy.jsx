947d8328cee8b776e662e48a96e1593e9ac72172
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-21 01:05:48 +0800

    Update Privacy.jsx

--- src/components/AuthComponents/Privacy.jsx
+++ src/components/AuthComponents/Privacy.jsx
@@ -17,7 +17,7 @@ const  Privacy = () => {
            <p className="forgot-password text-right">
               <Link to={"/login"}>Go Back to Login page ... </Link>
           </p>
-          <iframe src={privPdf}>
+          <iframe src={privPdf} width={1000} height={500}>
           </iframe>      
 
           
