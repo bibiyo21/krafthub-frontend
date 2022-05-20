621c7d2252ee7433ef70af82eeb73141b5f8d343
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-21 00:55:37 +0800

    Update Wrapper.jsx

--- src/components/AuthComponents/Wrapper.jsx
+++ src/components/AuthComponents/Wrapper.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import logoImg from '../../images/logo.jpg';
+import tagLineImg from '../../images/TagLine.jpg';
+import nameImg from '../../images/NameTag.jpg';
+import bg2Img from '../../images/Bg2.jpg';
+import { Link } from "react-router-dom";
 
 const Wrapper = ({ children }) => {
   if (localStorage.getItem('token')) {
@@ -6,12 +11,38 @@ const Wrapper = ({ children }) => {
   }
 
   return (
+    
+    <div>
+        <h2 class="text-left mb-3">
+          <img class="_1-kh _93s6 img" src={logoImg} alt="LogoKH" />
+            <ul class="_2fux">
+                <li class="_4qfn"><Link to={"/about"} class="_4vo5">About Us </Link>                                                     
+                </li>
+                <li class="_4qfn"><Link to={"/privacy"} class="_4vo5">Privacy Policy </Link>  
+                </li>
+                <li class="_4qfn"><Link to={"/terms"} class="_4vo5">Terms of use  </Link> 
+              </li>
+            </ul>
+        </h2>
     <div className="auth-wrapper">
-      <div className="auth-inner">
-        <h2 className="text-center mb-3">KraftHub</h2>
-        {children}
-      </div>
+              <div className="auth-inner">
+                {children}
+              </div>
+
+               <div className="auth-image">
+                <form> 
+                  <img src={bg2Img} alt="kraftHub" width="300" height="400" />
+                 </form>
+              </div>
+
+        </div>
+
+  
+
+
     </div>
+    
+        
   );
 };
 
